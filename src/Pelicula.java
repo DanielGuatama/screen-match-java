8a1af49e4774d3e69f0dce531e7fe3bbaea5423d
Daniel Guatama <edaniel.guatama@gmail.com>
2024-09-19 11:36:57 -0500

    Encapsulamiento

---
+++ src/Pelicula.java
@@ -0,0 +1,28 @@
+public class Pelicula {
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
+
+    int getTotalDeLasEvaluaciones() {
+        return totalDeLasEvaluaciones;
+    }
+
+    void muestraFichaTecnica() {
+        System.out.println("el nombre de la pelicula es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duraciòn en minutos" + duracionEnMinutos);
+    }
+
+    void evalua(double nota) {
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia() {
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
+}
